fad510a1d11bd6f3e8a5f3cc3b55119f18863e12
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2026-02-13 22:28:14 +0100

    Add configuration for enabling debug mode

--- src/Main.cs
+++ src/Main.cs
@@ -1,13 +1,28 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
 using UnityEngine;
 
 [BepInPlugin("com.oignontom8283.mechanicamultiplayerfix", "MechanicaMultiplayerFix", "1.0.0")]
 public class MechanicaMultiplayerFix : BaseUnityPlugin
 {
+    // Configuration mod entrys
+    public static ConfigEntry<bool> enableDebugMode;
+    
     void Awake()
     {
         Debug.Log("MechanicaMultiplayerFix is starting up...");
+
+        // Init BepInEx mod configuration
+        enableDebugMode = Config.Bind(
+            "General",                     // Section
+            "EnableDebugMode",             // Name key
+            false,                         // Default value
+            "Enable debug mode for the game (Theoretically incompatible with other mods)" // Description
+        );
+
+        Debug.Log("Debug mode is " + (enableDebugMode.Value ? "enabled" : "disabled"));
+        Debug.Log("MechanicaMultiplayerFix is now running!");
 
         var harmony = new Harmony("com.oignontom8283.mechanicamultiplayerfix");
         harmony.PatchAll();
