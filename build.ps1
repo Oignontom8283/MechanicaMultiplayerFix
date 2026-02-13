db24f12e036a0791add3bc3e6ddc6d5bedaad4eb
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2026-02-13 21:56:19 +0100

    Add initial project files including .gitignore, project configuration, build script, and main plugin code

---
+++ build.ps1
@@ -0,0 +1,2 @@
+dotnet build -c Release
+Copy-Item "bin\Release\netstandard2.1\MechanicaMultiplayerFix.dll" "C:\Program Files (x86)\Steam\steamapps\common\Mechanica\BepInEx\plugins\" -Force
---
+++ src/Main.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+[BepInPlugin("com.oignontom8283.mechanicamultiplayerfix", "MechanicaMultiplayerFix", "1.0.0")]
+public class MechanicaMultiplayerFix : BaseUnityPlugin
+{
+    void Awake()
+    {
+        Debug.Log("MechanicaMultiplayerFix is starting up...");
+
+        var harmony = new Harmony("com.oignontom8283.mechanicamultiplayerfix");
+        harmony.PatchAll();
+    }
+
+}
+
